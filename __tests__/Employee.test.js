2a74faef788b757afe7438831981bb618772e670
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-09-05 18:50:15 -0400

    fix ID count

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,9 @@
 const Employee = require('../lib/Employee.js');
 
 test('creates an employee object', () => {
-const employee = new Employee ('Phil','0000001','phil@pizza-overlord.com','Manager')
+const employee = new Employee ('Phil',0,'phil@pizza-overlord.com','Manager')
 expect(employee.name).toBe('Phil');
-expect(employee.id).toBe('0000001');
+expect(employee.id).toEqual(0);
 expect(employee.email).toBe('phil@pizza-overlord.com');
 expect(employee.role).toBe('Manager');
   });
--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const Manager = require('./lib/Manager.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
 
-new Intern().getSchool()
+new Employee().getId()

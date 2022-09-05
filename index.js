9c570780456ad954777a1b28867b8cd55a945242
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-09-05 18:08:41 -0400

    define constructors and add prompts

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const inquirer = require('inquirer');
+const Employee = require('./lib/Employee.js');
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+
+new Intern().getSchool()
